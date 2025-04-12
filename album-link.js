d41b18ed3c295f22b70732a9b1f8430275ce1bcf
DwyerGameDesign <dwyergamedesign@gmail.com>
2025-04-12 12:45:19 -0400

    adjust the position of hte level up box

--- album-link.js
+++ album-link.js
@@ -10,7 +10,7 @@ const ALBUM_URL = "https://www.alteredrootband.com/";
 const ALBUM_POSITION = [81.3, 45];
 
 // Dimensions of the album sprite
-const ALBUM_WIDTH = 60;
+const ALBUM_WIDTH = 90;
 const ALBUM_HEIGHT = 30;
 
 // Reference to the album element
