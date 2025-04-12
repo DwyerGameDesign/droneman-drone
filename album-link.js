088f13d59b97dc5be18424903a51c05cca9d4b34
DwyerGameDesign <dwyergamedesign@gmail.com>
2025-04-12 15:39:05 -0400

    adjust the album link

--- album-link.js
+++ album-link.js
@@ -10,8 +10,8 @@ const ALBUM_URL = "https://www.alteredrootband.com/";
 const ALBUM_POSITION = [81.3, 45]; // 81.3, 45
 
 // Dimensions of the album sprite
-const ALBUM_WIDTH = 90;
-const ALBUM_HEIGHT = 30;
+const ALBUM_WIDTH = 45;
+const ALBUM_HEIGHT = 26;
 
 // Reference to the album element
 let albumElement = null;
